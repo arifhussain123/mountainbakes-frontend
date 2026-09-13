44a9749063148c6976bbd69ea90ad9e3a1f8a82e
Arif Hussain <arifsiksavi@yahoo.com>
2026-09-13 11:34:20 +0500

    Revert "Restore Finance Help Desk page and nav entries"
    This reverts commit 52614b7139755150068275e5be0bba888e4360da.

--- src/app/(dashboard)/(finance)/finance-help-desk/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FinanceHelpDeskPage } from '@/components/finance/FinanceHelpDeskPage';
-
-export default function Page() {
-  return (
-    <div className="p-4 sm:p-6">
-      <FinanceHelpDeskPage />
-    </div>
-  );
-}
